914a51a721fa77b045434eb5913e0f2ad710d529
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-07-03 17:21:58 +0800

    Try out harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,18 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "" })
+		vim.keymap.set("n", "<leader>th", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "" })
+		vim.keymap.set("n", "<C-A>", function() harpoon:list():select(1) end, { desc = "" })
+		vim.keymap.set("n", "<C-B>", function() harpoon:list():select(2) end, { desc = "" })
+		vim.keymap.set("n", "<C-C>", function() harpoon:list():select(3) end, { desc = "" })
+		vim.keymap.set("n", "<C-D>", function() harpoon:list():select(4) end, { desc = "" })
+	end
+}
