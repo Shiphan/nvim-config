4d92269dc91a73de338772f6321031abeb67d244
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-07-13 16:20:18 +0800

    Switch to master branch of treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,19 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	branch = "main",
-	-- brranch = "master",
+	-- branch = "main",
+	brranch = "master",
 	lazy = false,
 	build = ":TSUpdate",
-	opt = {
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true},
+		})
+
+		-- TODO: idk how to use highlighting of treesitter main branch
+		-- vim.api.nvim_create_autocmd("FileType", {
+		--   callback = function() vim.treesitter.start() end,
+		-- })
+	end,
 }
