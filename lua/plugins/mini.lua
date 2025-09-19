2bbfe77d72a8d538719c1c7127de7fdc8a1f677c
Shiphan <140245703+Shiphan@users.noreply.github.com>
2025-09-19 20:57:58 +0800

    Remove mini.pairs and mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,20 +6,6 @@ return {
 		statusline.section_location = function()
 			return "%2l:%-2v"
 		end
-		require("mini.pairs").setup({})
-		require("mini.surround").setup({
-			mappings = {
-				add = "sa",
-				replace = "sr",
-				delete = "",
-				find = "",
-				find_left = "",
-				highlight = "",
-				update_n_lines = "",
-				suffix_last = "",
-				suffix_next = "",
-			},
-		})
 		require("mini.icons").setup({})
 	end,
 }
