d7216033cb5416e4d6e9259483d9a5451c858847
Shiphan <140245703+Shiphan@users.noreply.github.com>
2026-04-10 20:26:18 +0800

    Switch nvim-treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" }, -- TODO: check if main branch is ready for me
+	"https://github.com/nvim-treesitter/nvim-treesitter",
 })
 
 vim.api.nvim_create_autocmd("PackChanged", {
@@ -11,13 +11,45 @@ vim.api.nvim_create_autocmd("PackChanged", {
 	end,
 })
 
-require("nvim-treesitter.configs").setup({
-	auto_install = true,
-	highlight = { enable = true },
-	indent = { enable = true },
-})
+local highlight_autocmd_group = vim.api.nvim_create_augroup("treesitter_highlight", { clear = true })
+local function create_highlight_autocmd()
+	vim.api.nvim_create_autocmd("FileType", {
+		group = highlight_autocmd_group,
+		pattern = require("nvim-treesitter").get_installed(),
+		callback = function()
+			vim.treesitter.start()
+		end,
+	})
+end
+create_highlight_autocmd()
 
--- TODO: idk how to use highlighting of treesitter main branch
--- vim.api.nvim_create_autocmd("FileType", {
---   callback = function() vim.treesitter.start() end,
--- })
+local autoinstall_autocmd_group = vim.api.nvim_create_augroup("treesitter_autoinstall", { clear = true })
+local function create_autoinstall_autocmd()
+	local nvim_treesitter = require("nvim-treesitter")
+	local available = nvim_treesitter.get_available()
+	local installed = nvim_treesitter.get_installed()
+	local pattern = vim.tbl_filter(function(value)
+		return not vim.tbl_contains(installed, value)
+	end, available)
+
+	vim.api.nvim_create_autocmd("FileType", {
+		group = autoinstall_autocmd_group,
+		pattern = pattern,
+		callback = function(ev)
+			require("nvim-treesitter").install({ ev.match }):await(function(err, success)
+				if not err and success then
+					vim.treesitter.start()
+					create_highlight_autocmd()
+					create_autoinstall_autocmd()
+				end
+			end)
+		end,
+	})
+end
+create_autoinstall_autocmd()
+
+vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo[0][0].foldmethod = "expr"
+vim.opt.foldlevelstart = 99
+
+vim.bo.indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()'
