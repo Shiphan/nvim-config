fda3bd55ed0078f36ed84a1afd8d110efc367db8
Shiphan <140245703+Shiphan@users.noreply.github.com>
2026-04-10 20:27:42 +0800

    Add type hint for lsp config

--- after/lsp/tailwindcss.lua
+++ after/lsp/tailwindcss.lua
@@ -1,5 +1,6 @@
 local filetypes = dofile(vim.api.nvim_get_runtime_file("lsp/tailwindcss.lua", false)[1]).filetypes
 
+---@type vim.lsp.Config
 return {
 	filetypes = vim.list_extend(filetypes, {
 		"rust",
